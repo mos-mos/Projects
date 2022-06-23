3eff52b272dd36e9299f980fcbd2ca953782c2e1
mos <muyuntiandi@gmail.com>
2022-06-23 21:28:33 +0800

    vue

---
+++ _04_1-vue/src/Login.js
@@ -0,0 +1,5 @@
+const loginForm = {
+    template :' <div> <h2>登录页</h2>'+
+        '用户名:<input type="text" name="username"><br>'+
+        '密 码:<input type="password" name="password"><br> </div>'
+}
---
+++ _04_1-vue/src/register.js
@@ -0,0 +1,6 @@
+const registerForm = {
+    template :' <div> <h2>注册页</h2>'+
+        '用户名:<input type="text" name="username"><br>'+
+        '设置密码:<input type="password" name="password"><br>'+
+        '确认密码:<input type="password" name="password"><br> </div>'
+}
